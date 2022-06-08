dc32ade68b7295ef6dd7305a44aee4e09195f421
MohammadReza Shekohi <mrshekohi079@gmail.com>
2022-06-08 21:12:38 +0430

    Theme value will perserve its value between reloads

--- src/contexts/ThemeContextProvider.js
+++ src/contexts/ThemeContextProvider.js
@@ -1,5 +1,5 @@
 // This context will set the theme for the whole application whether it is DARK or BRIGHT
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
