335e7fdd5aff211b3686fb5651216e8a271cace1
MohammadReza Shekohi <mrshekohi079@gmail.com>
2022-06-08 10:39:49 +0430

    Loading skeleton placeholder was added to ProductDetails page for both DARK and LIGHT modes

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 // Context
 import { ProductContext } from '../contexts/ProductContextProvider';
@@ -23,8 +24,9 @@ const ProductDetails = ({ match }) => {
 
     const products = useContext(ProductContext);
     const { state, dispatch } = useContext(CartContext);
-    const {darkTheme} = useContext(ThemeContext);
+    const { darkTheme } = useContext(ThemeContext);
 
+    // This useEffect will render everytime when new data has been provided through context
     useEffect(() => {
         if (products.length > 0) {
             setProduct(products.find(product => product.id === parseInt(id)));
@@ -37,21 +39,66 @@ const ProductDetails = ({ match }) => {
     return (
         <div className={`${styles.container} ${darkTheme ? styles.dark : ""}`}>
             <div className={styles.imgContainer}>
-                <img src={image} alt="product" />
+                {
+                    products.length ?
+                        <img src={image} alt="product" /> :
+                        <Placeholder className={styles.textPlaceholder} style={{ width: "100%", height: "100%", }} />
+                }
             </div>
             <div className={styles.infoContainer}>
-                <h2 className={styles.title}>
-                    {title}
-                </h2>
-                <p className={styles.description}>{description}</p>
+                {
+                    products.length ?
+                        <h2 className={styles.title}>
+                            {title}
+                        </h2> :
+                        <Placeholder animation="wave" style={{}}>
+                            <Placeholder className={styles.textPlaceholder} xs={12} style={{ padding: "20px" }} />
+                        </Placeholder>
+                }
+                {
+                    products.length ?
+                        <p className={styles.description}>{description}</p> :
+                        <Placeholder animation="wave" style={{ textAlign: "left" }}>
+                            <Placeholder className={styles.textPlaceholder} xs={6} /> <Placeholder className={styles.textPlaceholder} xs={3} />
+                            <Placeholder className={styles.textPlaceholder} xs={5} /> {" "} <Placeholder className={styles.textPlaceholder} xs={2} />
+                            <br />
+                            <Placeholder className={styles.textPlaceholder} xs={4} />
+                        </Placeholder>
+                }
                 <div>
-                    <span className={styles.priceContainer}>قیمت: <span className={styles.price}>{price} $</span></span>
+                    <span className={styles.priceContainer}>
+                        {
+                            products.length ?
+                                <>
+                                    قیمت:
+                                    <span className={styles.price}>
+                                        {price} $
+                                    </span>
+                                </> :
+                                <Placeholder animation="wave" style={{ display: "block" }}>
+                                    <Placeholder
+                                        className={`${styles.textPlaceholder} ${styles.priceTitle}`}
+                                        style={{ width: "40px", padding: "5px" }}
+                                    /> 
+                                    {" "}
+                                    <Placeholder
+                                        className={`${styles.textPlaceholder} ${styles.pricePlaceholder}`}
+                                        style={{ width: "50px", padding: "13px 0" }}
+                                    />
+                                </Placeholder>
+                        }
+                    </span>
                     <div className={styles.buttons}>
                         {
                             productQuantityCart(state.items, parseInt(id)) === 0 ?
-                                <button onClick={() => dispatch({ type: "ADD_TO_CART", payload: product })} className={styles.addButton}>
-                                    افزودن به سبد خرید
-                                </button> :
+                                // If the data has yet to be loaded this code will 
+                                //make a placeholder for the button in state of loading data
+                                (products.length ?
+                                    <button onClick={() => dispatch({ type: "ADD_TO_CART", payload: product })} className={styles.addButton}>
+                                        افزودن به سبد خرید
+                                    </button> :
+                                    <Placeholder.Button className={styles.buttonPlaceholder} xs={12} />
+                                ) :
                                 <>
                                     <button className={`${styles.smallButton} ${styles.incButton}`} onClick={() => dispatch({ type: "INCREASE_ITEM", payload: product })}>
                                         <AddIcon />
